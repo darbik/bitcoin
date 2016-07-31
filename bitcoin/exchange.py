c4ab631c60582762b57470bcf9956bf3913989eb
darbik <dfesik1@gmail.com>
2016-07-31 21:06:53 +0000

    Basic sell flow skeleton for the exchange.

--- bitcoin/exchange.py
+++ bitcoin/exchange.py
@@ -8,3 +8,13 @@ def buy_bitcoin(price):
 
             amount = int(raw_input("Sorry please try another amount between $5 and $750."))
 
+def sell_bitcoin(price):
+
+    amount = int(raw_input("How much do you want to sell?"))
+
+    if amount > 750 or amount <= 5:
+
+        while amount > 750 or amount <= 5:
+
+            amount = int(raw_input("Sorry please try another amount between $5 and $750."))
+
