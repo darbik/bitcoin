aca6a453f996976a317195f5aa709d10435696e3
darbik <dfesik1@gmail.com>
2016-08-01 16:19:00 +0000

    Added a get_time() to record the date by atmid when a transaction is made.

--- bitcoin/metrics.py
+++ bitcoin/metrics.py
@@ -1,6 +1,14 @@
+from time import strftime
+
 def get_volume(atmid, price, amount):
     
     volume = (atmid, price, amount)
 
     return volume
+
+def get_time(atmid):
+
+    time = (atmid, strftime("%Y-%m-%d %H:%M:%S"))       # time format is in UTC
+
+    return time
 
