1bb2bcd2138dd7c3c0aca8f5d9de8fcb61e043a8
darbik <dfesik1@gmail.com>
2016-08-01 16:30:14 +0000

    Added proper variables to indicate if a transaction is using a buy or sell operation. Will be used for a function to update the balance of an atm.

--- bitcoin/exchange.py
+++ bitcoin/exchange.py
@@ -7,12 +7,14 @@ def buy_bitcoin(price):
         while amount > 750 or amount <= 5:  # need to manage bill denominations in the safe
 
             amount = int(raw_input("Sorry please try another amount between $5 and $750."))
+    
+    amount = ('buy', amount)
 
     return amount
 
 def sell_bitcoin(price):
 
-    amount = int(raw_input("How much do you want to sell?"))
+    amount = ('sell', int(raw_input("How much do you want to sell?")))
 
     if amount > 750 or amount <= 5:
 
@@ -20,5 +22,7 @@ def sell_bitcoin(price):
 
             amount = int(raw_input("Sorry please try another amount between $5 and $750."))
     
+    amount = ('sell', amount)
+
     return amount
 
