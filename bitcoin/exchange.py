fb02d0f2bc2e4de2b2e1da557db9a5951d9a8a2f
darbik <dfesik1@gmail.com>
2016-08-08 14:55:28 +0000

    Edited exchange.py to change max transaction to 500 from 750 dollars.

--- bitcoin/exchange.py
+++ bitcoin/exchange.py
@@ -2,11 +2,11 @@ def buy_bitcoin(price):
 
     amount = int(raw_input("How much do you want to buy?"))
 
-    if amount > 750 or amount <= 5:
+    if amount > 500 or amount < 5:
 
-        while amount > 750 or amount <= 5:  # need to manage bill denominations in the safe
+        while amount > 500 or amount < 5: 
 
-            amount = int(raw_input("Sorry please try another amount between $5 and $750."))
+            amount = int(raw_input("Sorry please try another amount between $5 and $500."))
     
     amount = ('buy', amount)
 
@@ -16,11 +16,11 @@ def sell_bitcoin(price):
 
     amount = int(raw_input("How much do you want to sell?"))
 
-    if amount > 750 or amount <= 5:
+    if amount > 500 or amount < 5:
 
-        while amount > 750 or amount <= 5:
+        while amount > 500 or amount < 5:
 
-            amount = int(raw_input("Sorry please try another amount between $5 and $750."))
+            amount = int(raw_input("Sorry please try another amount between $5 and $500."))
     
     amount = ('sell', amount)
 
@@ -34,7 +34,7 @@ def calculate_fees(atmAmount):
 
 def display_transaction(amount, price):
 
-    operation, amount = (amount)
+    operation, amount = amount
     
     if operation == 'buy':
 
